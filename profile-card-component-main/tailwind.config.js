5b9902d1008e82f720146b0ad480ffca5bca2c42
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-02-16 22:11:38 +0100

    almost

--- profile-card-component-main/tailwind.config.js
+++ profile-card-component-main/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*{html,js,css}"],
+  content: ["./builid/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkGray: 'hsl(0, 0%, 59%)',
+        DarkCyan: "hsl(185, 75%, 39%)",
+        VeryDarkDesaturatedBlue: "hsl(229, 23%, 23%)",
+        DarkGrayishBlue: "hsl(227, 10%, 46%)"
+      }
+    },
   },
   plugins: [],
 }
