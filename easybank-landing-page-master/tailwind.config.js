b09405beefa589a63de1d64c9000994ee1d7a1e9
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-02-19 22:48:57 +0100

    footer

--- easybank-landing-page-master/tailwind.config.js
+++ easybank-landing-page-master/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+            colors:{
+              GrayishBlue: "hsl(233, 8%, 62%)",
+              LightGrayishBlue: "hsl(220, 16%, 96%)",
+              VeryLightGray: "hsl(0, 0%, 98%)",
+              White: "hsl(0, 0%, 100%)",
+              DarkBlue: "hsl(233, 26%, 24%)",
+              LimeGreen: "hsl(136, 65%, 51%)",
+              BrightCyan: "hsl(192, 70%, 51%)",
+              },
+            fontFamily:{
+              'customFont':['Public', 'Sans']
+            }
+            },
   },
   plugins: [],
 }
